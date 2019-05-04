52c38a580ab7bf73e5b8cf60012cc08f38b46e2c
ocramz <zocca.marco@gmail.com>
2019-05-04 18:03:10 +0200

    omitting BM for now

--- bench/Time.hs
+++ bench/Time.hs
@@ -7,4 +7,8 @@ import Criterion.Main.Options (defaultConfig)
 import Criterion.Measurement (initializeTime, secs)
 import Criterion.Monad (withConfig)
 
+
+import qualified Data.Trie.AlexanderGreen as AG
+import qualified Data.Trie.JustinLe as JL
+
 main = putStrLn "bench time : unimplemented"
--- src/Data/Trie/BartoszMilewski.hs
+++ src/Data/Trie/BartoszMilewski.hs
@@ -7,8 +7,17 @@ import Prelude hiding (lookup)
 import qualified Prelude as P (lookup)
 
 
-newtype TrieF a x = TrieF [(a, x)] deriving (Show, Functor)
 
+newtype T2F k a x = T2F [Row k a x] deriving (Show, Functor)
+data Row k a x = Row k a x deriving (Show, Functor)
+newtype T2 k a = T2 (Fix (T2F k a)) deriving Show
+cataT2 :: (T2F k a x -> x) -> T2 k a -> x
+cataT2 phi (T2 t) = cata phi t
+anaT2 :: (x -> T2F k a x) -> x -> T2 k a
+anaT2 psi z = T2 $ ana psi z
+
+
+newtype TrieF a x = TrieF [(a, x)] deriving (Show, Functor)
 newtype Trie a = Trie (Fix (TrieF a)) deriving (Show)
 
 
@@ -22,15 +31,15 @@ hyloT phi psi = cataT phi . anaT psi
 
 
 
--- -- lookup :: Ord k => [k] -> Trie k v -> Maybe v
--- lookup ks = cataT (lookupAlg ks)
+-- -- -- lookup :: Ord k => [k] -> Trie k v -> Maybe v
+-- lookup ks t = cataT lookupAlg t ks
 
--- lookupAlg :: Eq a => [a] -> TrieF a [b] -> [b]
--- lookupAlg kss (TrieF vs) = case kss of
+-- lookupAlg :: Eq a1 => TrieF a1 ([a1] -> [a2]) -> [a1] -> [a2]
+-- lookupAlg (TrieF vs) kss = case kss of
 --   [] -> []
---   k:_ -> case P.lookup k vs of
+--   k:ks -> case P.lookup k vs of
 --     Nothing -> []
---     -- Just x -> [x]
+--     Just lf -> lf ks
 
 
 
