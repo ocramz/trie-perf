c80534569455a1d84231434bcc0cd24b3968d23e
ocramz <zocca.marco@gmail.com>
2019-05-04 19:20:58 +0200

    add randomized time benchmarks

--- bench/Time.hs
+++ bench/Time.hs
@@ -1,52 +1,78 @@
 module Main where
 
+import Control.Monad (replicateM)
+
 import Criterion (Benchmarkable)
 import Criterion.Types (Benchmark (..), Report (..), DataRecord( Analysed ), Config (..), SampleAnalysis (..), Verbosity (..), Regression (..))
 import Criterion.Internal (runAndAnalyseOne)
-import Criterion.Main (defaultMain, bgroup, bench, whnf)
+import Criterion.Main (defaultMain, bgroup, bench, whnf, whnfIO)
 import Criterion.Main.Options (defaultConfig)
 import Criterion.Measurement (initializeTime, secs)
 import Criterion.Monad (withConfig)
 
-import Data.List
+-- import Data.List
+
+import Control.Monad.Primitive
+import System.Random.MWC.Probability (create, withSystemRandom, samples, discreteUniform, Prob, Gen, GenIO)
 
 import qualified Data.Trie.AlexanderGreen as AG (Trie, Key, fromList, lookup)
 import qualified Data.Trie.JustinLe as JL (Trie, fromList, lookup)
 
--- main = putStrLn "bench time : unimplemented"
 
 
-ag :: (Foldable t, Eq a) => (t (AG.Key a, b), AG.Key a) -> Maybe b
+ag :: Eq a => ([(AG.Key a, b)], AG.Key a) -> Maybe b
 ag (kxs, k) = AG.lookup k $ AG.fromList kxs
 
 jl :: Ord k => ([([k], v)], [k]) -> Maybe v
 jl (kxs, k) = JL.lookup k $ JL.fromList kxs
 
 
--- wordss n = [w] : w where
---   (w, ws) = splitAt n ['a' .. 'z']
+letters :: [Char]
+letters = ['a' .. 'z']
 
--- wordss n = go [] ['a' .. 'z'] where
---   go acc w = (ws : acc) ++ go wss
---     where (ws, wss) = splitAt n w
-    
+-- randStrings :: Int -> Int -> IO [String] -- 
+-- randStrings m n = replicateM m (withSystemRandom io) where
+--   io :: GenIO -> IO String
+--   io g = samples n (discreteUniform letters) g
+
+randStrings :: PrimMonad m => Int -> Int -> Gen (PrimState m) -> m [String]
+randStrings m n g = replicateM m (samples n (discreteUniform letters) g)
+
+nth :: Int -> [c] -> c    
+nth n = head . drop (n-1)
+
+randTrieInput :: PrimMonad m =>
+                 Int  -- ^ # of random strings
+              -> Int  -- ^ length of strings
+              -> Int  -- ^ position of key 
+              -> Gen (PrimState m)
+              -> m ([(String, Int)], String)
+randTrieInput m n i g = do
+  ss <- randStrings m n g
+  let k = nth i ss
+  pure (zip ss [0 ..], k)
 
 
+     
 
-fib :: Int -> Int
-fib m | m < 0     = error "negative!"
-      | otherwise = go m
-  where go 0 = 0
-        go 1 = 1
-        go n = go (n-1) + go (n-2)
-
-main = defaultMain [
+main :: IO ()
+main = withSystemRandom $ \g -> defaultMain [
   bgroup "AG" [
-        bench "1" $ whnf ag ([("mo",1)], "mo")
-      , bench "10" $ whnf ag ([("ma",1), ("mb",2), ("mc",1), ("md",2), ("me",1), ("mf",2), ("mg",1), ("mh",2), ("mi",1), ("mj",2)], "mi")
-              ],
+      bench "3/3/1" $ whnfIO $ do
+          x <- randTrieInput 3 3 1 g
+          pure $ ag x
+    , bench "30/30/10" $ whnfIO $ do
+          x <- randTrieInput 30 30 10 g
+          pure $ ag x     
+              ]
+  ,
   bgroup "JL" [
-        bench "1" $ whnf jl ([("mo",1)], "mo")
-      , bench "10" $ whnf jl ([("ma",1), ("mb",2), ("mc",1), ("md",2), ("me",1), ("mf",2), ("mg",1), ("mh",2), ("mi",1), ("mj",2)], "mi")
-              ]    
+      bench "3/3/1" $ whnfIO $ do
+          x <- randTrieInput 3 3 1 g
+          pure $ jl x
+    , bench "30/30/10" $ whnfIO $ do
+          x <- randTrieInput 30 30 10 g
+          pure $ jl x     
+              ]  
+    
   ]
